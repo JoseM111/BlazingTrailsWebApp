aead8850f97a591ba18c1ebf76c609275b11b860
Jose Martinez <alias_one11@outlook.com>
2021-04-29 13:13:07 +0200

    1. First commit...

---
+++ Features/Home/Models/TrailModel.cs
@@ -0,0 +1,12 @@
+namespace BlazingTrailsWebApp.Features.Home.Models 
+{
+	public class TrailModel {
+		#region - ™PROPERTIES™ ☰☰☰☰☰☰☰☰☰☰
+		public string Name { get; set; }
+		public string Image { get; set; }
+		public string Location { get; set; }
+		public string Time { get; set; }
+		public int Length { get; set; }
+		#endregion ™☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Part #2
+
+### What we will cover
+* Options for structuring components
+* Lifecycle methods
+* Handling DOM events
+* Passing values between components
+* Component styling
